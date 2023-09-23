e829b2859714a85fdbc58fd90c883ea0d49129d6
Tillymolete <Mahlatsemolete@gmail.com>
2023-09-23 12:01:10 -0700

    bubblesort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * @array: the pointer of an array
+ * @size: the size of the array
+ * Retrun: Nothing
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t a, b;
+	int swapped;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (a = 0; a < size - 1; a++)
+	{
+		swappeed = 0;
+
+		for (b = 0; b < size -1 - a; b++)
+		{
+			if (array[b] > array[b + 1])
+			{
+				swap(&array[b], &array[b + 1]);
+				swapped = 1;
+			}
+		}
+		if (swapped == 0)
+			break;
+	}
+}
+
+/**
+ * swap - swaps two items
+ * @array_x: the first item
+ * @array_y: the second array
+ * Return: Nothing
+ */
+
+void swap(int *array_x, int *array_y)
+{
+	int tmp = *array_x;
+	
+	*array_x = *array_y;
+	array_y = tmp;
+}
